20cab811e4e94b37f14ebbcb72177b9093447233
unknown <pararahul01@gmail.com>
2020-12-26 17:03:10 +0530

    Init commit

---
+++ js/loader.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".modal").modal();
+  $(".parallax").parallax();
+  $(".sidenav").sidenav();
+  $(".slider").slider({ full_width: true });
+  $(".parallax").parallax();
+  $(".myreviews").carousel({
+    numVisible: 5,
+    shift: 55,
+    padding: 55,
+  });
+});
+
+function toggleModal() {
+  var instance = M.Modal.getInstance($("#modal3"));
+  instance.open();
+}
+
+
+
+function onclickk(){
+    document.getElementById("here").innerHTML = "FurtherDetails of the image";
+    
+
+}
